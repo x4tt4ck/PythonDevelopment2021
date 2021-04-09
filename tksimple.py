47345790131faa5c134b96c07e16bc6646f3c873
Phi <phi@01ab.wiki>
2021-04-09 16:01:43 +0300

    Geometry fixes, super() used

--- tksimple.py
+++ tksimple.py
@@ -10,17 +10,18 @@ import time
 
 class Application(tk.Frame):
     def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
+        super().__init__(master)
         self.grid()
         self.createWidgets()
 
     def createWidgets(self):
         self.quitButton = tk.Button(self, text="Quit", command=self.quit)
         self.timeButton = tk.Button(self, text="Time", command=self.settime)
-        self.timeLabel = tk.Label(self, text="<time appears here>")
-        self.quitButton.grid(row=0, column=0)
+        self.timeLabel = tk.Label(self)
+        self.quitButton.grid()
         self.timeButton.grid(row=0, column=1)
         self.timeLabel.grid(columnspan=2)
+        self.settime()
 
     def settime(self):
         self.timeLabel["text"] = time.strftime("%c")
