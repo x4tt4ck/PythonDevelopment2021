883c7b6929755bd2281a4093324236ae31a7f3a9
Phi <phi@01ab.wiki>
2021-04-09 15:55:11 +0300

    Two buttons and a label are added

--- tksimple.py
+++ tksimple.py
@@ -2,9 +2,11 @@
 
 '''
 TkInter example
+Launches a window with "Quit" button.
 '''
 
 import tkinter as tk
+import time
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -13,8 +15,15 @@ class Application(tk.Frame):
         self.createWidgets()
 
     def createWidgets(self):
-        self.quitButton = tk.Button(self, text='Quit', command=self.quit)
+        self.quitButton = tk.Button(self, text="Quit", command=self.quit)
+        self.timeButton = tk.Button(self, text="Time", command=self.settime)
+        self.timeLabel = tk.Label(self, text="<time appears here>")
         self.quitButton.grid()
+        self.timeButton.grid()
+        self.timeLabel.grid()
+
+    def settime(self):
+        self.timeLabel["text"] = time.strftime("%c")
 
 app = Application()
 app.master.title('Sample application')
